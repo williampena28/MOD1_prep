aa5fc84c18d6be3f19e2136f1a107b346f7145bc
williampena28 <56842242+williampena28@users.noreply.github.com>
2022-12-13 13:52:35 -0500

    created classes Person and FriendGroup

---
+++ index.js
@@ -0,0 +1,32 @@
+class Person 
+{
+    constructor(name, confidence, requiredConfidence, charisma)
+    {
+        this.name = name;
+        this.confidence = confidence;
+        this.requiredConfidence = requiredConfidence;
+        this.charisma = charisma;
+    }
+    convince()
+    {
+
+    }
+    discourage()
+    {
+
+    }
+}
+
+class FriendGroup
+{
+    constructor()
+    {
+
+    }
+    addFriend()
+    {
+
+    }
+}
+
+let me = new Person("Abruce", 100, 30, 20);
